60e6145238e818af06811441254e7d072ee7a57c
Oscar Chavez - Terabit Lab <oscarchb04@gmail.com>
2022-07-15 12:34:28 -0400

    *Add host worker background app

--- ConsoleWorkerFullTest/ConsoleWorkerFullTest/Program.cs
+++ ConsoleWorkerFullTest/ConsoleWorkerFullTest/Program.cs
@@ -1,38 +1,67 @@
 ﻿using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace ConsoleWorkerFullTest;
+using ConsoleWorkerFullTest;
 
-class Program
-{
-    #region Injections
-
-    private static readonly ServiceProvider _serviceProvider
-                = new ServiceCollection()
-                .AddTransient<Program>()
-                .AddTransient<IRepo, Repo>()
-                .AddTransient<IServiceRepo, ServiceRepo>()
-                .AddLogging(configure =>
-                    configure.AddFilter("Microsoft", LogLevel.Warning)
-                        .AddFilter("System", LogLevel.Warning)
-                        .AddFilter("NonHostConsoleApp.Program", LogLevel.Debug)
-                        .AddConsole()
-                )
-                .BuildServiceProvider();
-
-    #endregion
-
-    #region Ctor
-
-    private readonly IServiceRepo _serviceRepo;
-
-    public Program(IServiceRepo serviceRepo)
+using IHost host = CreateHostBuilder(args)
+    .ConfigureServices((hostContext, services) =>
     {
-        _serviceRepo = serviceRepo ?? throw new ArgumentNullException(nameof(serviceRepo));
-    }
+        services.AddHostedService<Worker>();
+    })
+    .Build();
 
-    #endregion
+await host.RunAsync();
 
-    static void Main()
-        => _serviceProvider.GetRequiredService<IServiceRepo>().ShowMessage();
-}
+
+static IHostBuilder CreateHostBuilder(string[] args) =>
+    Host.CreateDefaultBuilder(args)
+    .ConfigureServices((_, services) =>
+    {
+        services.AddTransient<Worker>();
+    })
+    .ConfigureLogging((_, logging) =>
+    {
+        logging.ClearProviders();
+        logging.AddSimpleConsole(options => options.IncludeScopes = true);
+        logging.AddEventLog();
+    });
+
+
+
+/// <summary>
+/// Example with generic console
+/// </summary>
+//class Program
+//{
+//    #region Injections
+
+//    private static readonly ServiceProvider _serviceProvider
+//                = new ServiceCollection()
+//                .AddTransient<Program>()
+//                .AddTransient<IRepo, Repo>()
+//                .AddTransient<IServiceRepo, ServiceRepo>()
+//                .AddLogging(configure =>
+//                    configure.AddFilter("Microsoft", LogLevel.Warning)
+//                        .AddFilter("System", LogLevel.Warning)
+//                        .AddFilter("NonHostConsoleApp.Program", LogLevel.Debug)
+//                        .AddConsole()
+//                )
+//                .BuildServiceProvider();
+
+//    #endregion
+
+//    #region Ctor
+
+//    private readonly IServiceRepo _serviceRepo;
+
+//    public Program(IServiceRepo serviceRepo)
+//    {
+//        _serviceRepo = serviceRepo ?? throw new ArgumentNullException(nameof(serviceRepo));
+//    }
+
+//    #endregion
+
+//    static void Main()
+//        => _serviceProvider.GetRequiredService<IServiceRepo>().ShowMessage();
+//}
