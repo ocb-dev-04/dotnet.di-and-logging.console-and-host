cf772158dc36571a0d2e124c03cc86c1308e4009
Oscar Chavez - Terabit Lab <oscarchb04@gmail.com>
2022-07-15 12:19:16 -0400

    *Add basic example with console

---
+++ ConsoleWorkerFullTest/ConsoleWorkerFullTest/Program.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ConsoleWorkerFullTest;
+
+class Program
+{
+    #region Injections
+
+    private static readonly ServiceProvider _serviceProvider
+                = new ServiceCollection()
+                .AddTransient<Program>()
+                .AddTransient<IRepo, Repo>()
+                .AddTransient<IServiceRepo, ServiceRepo>()
+                .AddLogging(configure =>
+                    configure.AddFilter("Microsoft", LogLevel.Warning)
+                        .AddFilter("System", LogLevel.Warning)
+                        .AddFilter("NonHostConsoleApp.Program", LogLevel.Debug)
+                        .AddConsole()
+                )
+                .BuildServiceProvider();
+
+    #endregion
+
+    #region Ctor
+
+    private readonly IServiceRepo _serviceRepo;
+
+    public Program(IServiceRepo serviceRepo)
+    {
+        _serviceRepo = serviceRepo ?? throw new ArgumentNullException(nameof(serviceRepo));
+    }
+
+    #endregion
+
+    static void Main()
+        => _serviceProvider.GetRequiredService<IServiceRepo>().ShowMessage();
+}
---
+++ ConsoleWorkerFullTest/ConsoleWorkerFullTest/ServiceRepo.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace ConsoleWorkerFullTest;
+
+public sealed class ServiceRepo : IServiceRepo
+{
+    #region Props & Ctor
+
+    private readonly IRepo _repo;
+    private readonly ILogger<ServiceRepo> _logger;
+    
+    public ServiceRepo(
+        IRepo repo,
+        ILogger<ServiceRepo> logger)
+    {
+        _repo = repo;
+        _logger = logger;
+    }
+
+    #endregion
+    
+    public void ShowMessage()
+    {
+        _logger.LogWarning($"Ejecute method {nameof(ShowMessage)} into {nameof(ServiceRepo)}");
+        _repo.ShowMessage();
+    }
+}
+
+public interface IServiceRepo
+{
+    void ShowMessage();
+}
